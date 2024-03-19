f514f50dfcaba5c24fe8e02beb8e3645216ffbd4
jamesthecooke <jamesjcooke03@gmail.com>
2024-03-19 16:07:05 +0000

    updated put function for users by id

--- next-app/app/api/users/[id]/route.tsx
+++ next-app/app/api/users/[id]/route.tsx
@@ -19,7 +19,7 @@ export async function GET(
 
 export async function PUT(
   request: NextRequest,
-  { params }: { params: { id: number } }
+  { params }: { params: { id: string } }
 ) {
   //  Validate the rewuest body
   // if invalid return 400
@@ -33,10 +33,22 @@ export async function PUT(
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
 
-  if (params.id > 10)
+  const user = await prisma.user.findUnique({
+    where: { id: parseInt(params.id) },
+  });
+
+  if (!user)
     return NextResponse.json({ error: "User not found " }, { status: 404 });
 
-  return NextResponse.json({ id: 1, name: body.name });
+  const updatedUser = await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+
+  return NextResponse.json({ updatedUser });
 }
 
 export function DELETE(
