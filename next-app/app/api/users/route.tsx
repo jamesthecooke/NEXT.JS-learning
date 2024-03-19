ffcef96596f6c7367e42e42803b5f51a961643df
jamesthecooke <jamesjcooke03@gmail.com>
2024-03-19 15:58:38 +0000

    added post function for users api to create new users

--- next-app/app/api/users/route.tsx
+++ next-app/app/api/users/route.tsx
@@ -20,6 +20,23 @@ export async function POST(request: NextRequest) {
   // else, return the data tht was created
   if (!body.name)
     return NextResponse.json({ error: "Name is required" }, { status: 400 });
-  return NextResponse.json({ id: 1, name: body.name }, { status: 201 });
+
+  if (!body.email)
+    return NextResponse.json({ error: "Email is required" }, { status: 400 });
+
+  const user = await prisma.user.findUnique({
+    where: { email: body.email },
+  });
+
+  if (user)
+    return NextResponse.json({ error: "User already exists" }, { status: 400 });
+
+  const newUser = await prisma.user.create({
+    data: {
+      name: body.name,
+      email: body.email,
+    },
+  });
+  return NextResponse.json(newUser, { status: 201 });
 }
 //put - updating data
